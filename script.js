9ab86df5d03b064f604d089f22ec4dc4a170f548
Edilson Rodrigues dos Santos <erds-tecnologo@outlook.com.br>
2021-04-12 23:20:56 -0300

    Definindo Movimentos

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-
+let direction = "right";
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
@@ -18,5 +18,27 @@ function criarBG(){
      }
 
  }
-criarBG();
-criarCobrinha();
+
+ function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "right") snakeY += box;
+    if(direction == "left") snakeY  -= box;
+
+    snake.pop();
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+ }
+
+ let jogo = setInterval(iniciarJogo, 100);
